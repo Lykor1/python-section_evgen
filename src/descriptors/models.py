ba31c302488904eebb96551b09a9e80f82f5be69
Evgen <evgeshkarach@gmail.com>
2025-09-13 13:46:19 +0400

    2 задание выполнено

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,27 @@ class Model:
 
 
 class Field:
-    pass
+    def __init__(self, path):
+        self.path = path.split('.')
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        data = instance.payload
+        for key in self.path:
+            if key in data:
+                data = data[key]
+            else:
+                return None
+        return data
+
+    def __set__(self, instance, value):
+        data = instance.payload
+        for key in self.path[:-1]:
+            if key in data:
+                data = data[key]
+            else:
+                return
+        last_key = self.path[-1]
+        if last_key in data:
+            data[last_key] = value
